3eddc090ae8f8af1cf82523d2e4ecd62dcef0407
Alberto Apure <apure@q.com>
2015-06-23 23:01:02 -0600

    Create greet-user.php

---
+++ greet-user.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Greet User
+ * Author: Peter Suhm
+ * Version: 0.0.1
+ */
+$checkForGreetingFilter = function() {
+    if (isset($_GET['hello'])) {
+        wp_die("<h1>Hello {$_GET['hello']}!</h1>");
+    }
+};
